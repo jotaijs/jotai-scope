91c176f142757d19d33e5aa546083626e87e9c19
David Maskasky <dmaskasky@otter.ai>
2024-07-19 19:26:04 -0700

    refactor jotai-scope to use the new store.unstable_derive api

--- src/ScopeProvider/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Atom, WritableAtom, getDefaultStore } from 'jotai';
-
-export type AnyAtom = Atom<unknown> | WritableAtom<unknown, unknown[], unknown>;
-export type AnyWritableAtom = WritableAtom<unknown, unknown[], unknown>;
-export type Store = ReturnType<typeof getDefaultStore>;
