e5346d35fb08951dcbe7a4160dcccad9c3c691bf
David Maskasky <dmaskasky@otter.ai>
2024-09-24 16:38:52 -0700

    chore(tests): update references to tests to point to local jotai instance

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,4 +1,6 @@
 import { fireEvent } from '@testing-library/react'
+import { Store } from 'src/ScopeProvider/types'
+import { INTERNAL_DevStoreRev4, INTERNAL_PrdStore } from 'jotai/vanilla/store'
 
 function getElements(container: HTMLElement, querySelectors: string[]): Element[] {
   return querySelectors.map((querySelector) => {
@@ -20,4 +22,31 @@ export function clickButton(container: HTMLElement, querySelector: string) {
     throw new Error(`Button not found: ${querySelector}`)
   }
   fireEvent.click(button)
+}
+
+export function getDevStore(store: Store): INTERNAL_PrdStore & INTERNAL_DevStoreRev4 {
+  if (!isDevStore(store)) {
+    throw new Error('Store is not a dev store')
+  }
+  return store
+}
+
+export function isDevStore(store: Store): store is INTERNAL_PrdStore & INTERNAL_DevStoreRev4 {
+  return (
+    'dev4_get_internal_weak_map' in store &&
+    'dev4_get_mounted_atoms' in store &&
+    'dev4_restore_atoms' in store
+  )
+}
+
+export function assertIsDevStore(
+  store: Store,
+): asserts store is INTERNAL_PrdStore & INTERNAL_DevStoreRev4 {
+  if (!isDevStore(store)) {
+    throw new Error('Store is not a dev store')
+  }
+}
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
