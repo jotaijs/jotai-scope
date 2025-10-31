f4fe52daf4727b0e38c0006468ade3c911571e8e
David Maskasky <dmaskasky@gmail.com>
2025-10-31 11:50:11 -0700

    fix: building block patching (#91)
    * fix: building block patching
    
    * update version to 0.9.7

--- tests/issues.test.ts
+++ tests/issues.test.ts
@@ -35,41 +35,4 @@ describe('open issues', () => {
     expect(a.read).toHaveBeenCalledTimes(1)
     expect(a.onMount).toHaveBeenCalledTimes(1)
   })
-
-  it('https://github.com/jotaijs/jotai-scope/issues/62', () => {
-    const a = atom(0)
-    a.debugLabel = 'atomA'
-
-    const b = atom(null, (_, set, value: number) => {
-      set(a, value)
-    })
-    b.debugLabel = 'atomB'
-
-    const c = atom(null, (_, set, value: number) => {
-      set(b, value)
-    })
-    c.debugLabel = 'atomC'
-
-    function createStores() {
-      const s0 = createDebugStore()
-      s0.sub(a, () => {
-        console.log('S1', s0.get(a))
-      })
-
-      const s1 = createScope({
-        atoms: [a],
-        parentStore: s0,
-        name: 's1',
-      })
-      s1.sub(a, () => {
-        console.log('S1', s1.get(a))
-      })
-      return { s0Store: s0, s1Store: s1 }
-    }
-    {
-      const { s0Store, s1Store } = createStores()
-      s1Store.set(c, 1)
-      expect([s0Store.get(a), s1Store.get(a)] + '').toEqual('0,1')
-    }
-  })
 })
