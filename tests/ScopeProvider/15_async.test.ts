0d775d9ffae4a1e5ff30fa93e53380fd4c4fd7a8
David Maskasky <dmaskasky@gmail.com>
2025-12-11 20:17:06 -0800

    add a new utility - markdependent

--- tests/ScopeProvider/15_async.test.ts
+++ tests/ScopeProvider/15_async.test.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai'
 import { describe, expect, test } from 'vitest'
+import { markDependent } from 'jotai-scope'
 import { createScopes, delay } from '../utils'
 
 describe('async', () => {
@@ -20,8 +21,31 @@ describe('async', () => {
     s[1].set(a, 1)
     expect(await s[0].get(c)).toBe('10')
     expect(await s[1].get(c)).toBe('10')
-    s[0].set(b, 1)
-    expect(await s[0].get(c)).toBe('11')
+    s[1].set(b, 1)
+    expect(await s[0].get(c)).toBe('10')
+    expect(await s[1].get(c)).toBe('11')
+  })
+
+  test('should work with async atoms and markDependent', async () => {
+    const a = atom(0)
+    a.debugLabel = 'a'
+    const b = atom(0)
+    b.debugLabel = 'b'
+    const c = atom(async (get) => {
+      const aValue = get(a)
+      await delay(0)
+      const bValue = get(b)
+      return '' + aValue + bValue
+    })
+    c.debugLabel = 'c'
+    markDependent(c, [a, b])
+    const s = createScopes([b])
+    s[1].sub(c, () => {})
+    s[1].set(a, 1)
+    expect(await s[0].get(c)).toBe('10')
     expect(await s[1].get(c)).toBe('10')
+    s[1].set(b, 1)
+    expect(await s[0].get(c)).toBe('10')
+    expect(await s[1].get(c)).toBe('11')
   })
 })
