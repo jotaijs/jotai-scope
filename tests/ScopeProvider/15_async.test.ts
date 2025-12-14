8e7c4a64eb5c5fe4176715c0d850d74eb5b99710
David Maskasky <dmaskasky@gmail.com>
2025-12-13 22:43:50 -0800

    add notes and failing test for async case TODO 1: baseStore should not call readAtomState on scoped atoms TODO 2: async scope classification is possible with recompute and change store hooks

--- tests/ScopeProvider/15_async.test.ts
+++ tests/ScopeProvider/15_async.test.ts
@@ -16,6 +16,10 @@ describe('async', () => {
       return '' + aValue + bValue
     })
     c.debugLabel = 'c'
+    /**```
+      S0[_]: a0, b0, c0(a0 b0 then)
+      S1[b]: a0, b1, c1(a0 b1 then)
+    */
     const s = createScopes([b])
     s[1].sub(c, () => {})
     s[1].set(a, 1)
@@ -39,6 +43,36 @@ describe('async', () => {
     })
     c.debugLabel = 'c'
     markDependent(c, [a, b])
+    /**```
+      S0[_]: a0, b0, c0(a0 then b0)
+      S1[b]: a0, b1, c1(a0 then b1)
+    */
+    const s = createScopes([b])
+    s[1].sub(c, () => {})
+    s[1].set(a, 1)
+    expect(await s[0].get(c)).toBe('10')
+    expect(await s[1].get(c)).toBe('10')
+    s[1].set(b, 1)
+    expect(await s[0].get(c)).toBe('10')
+    expect(await s[1].get(c)).toBe('11')
+  })
+
+  test('should work with async atoms', async () => {
+    const a = atom(0)
+    a.debugLabel = 'a'
+    const b = atom(0)
+    b.debugLabel = 'b'
+    const c = atom(async (get) => {
+      const aValue = get(a)
+      await delay(0)
+      const bValue = get(b)
+      return '' + aValue + bValue
+    })
+    c.debugLabel = 'c'
+    /**```
+      S0[_]: a0, b0, c0(a0 then b0)
+      S1[b]: a0, b1, c1(a0 then b1)
+    */
     const s = createScopes([b])
     s[1].sub(c, () => {})
     s[1].set(a, 1)
