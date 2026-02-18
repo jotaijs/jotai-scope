a125826f463e9e5be407dc5a9f168135f8290685
daishi <daishi@axlight.com>
2026-02-19 01:12:57 +0900

    fix: backward compat for onInit

--- src/ScopeProvider/scope.ts
+++ src/ScopeProvider/scope.ts
@@ -251,7 +251,13 @@ function createPatchedStore(scope: Scope): Store {
   const atomOnInit = storeState[9]
   const alreadyPatched: StoreHooks = {}
 
-  storeState[9] = (_, atom) => atomOnInit(scopedStore, atom)
+  storeState[9] = (_, atom) => {
+    atomOnInit(scopedStore, atom)
+    // backward compat: call unstable_onInit if INTERNAL_onInit is not present
+    if (!(atom as any).INTERNAL_onInit && (atom as any).unstable_onInit) {
+      ;(atom as any).unstable_onInit(scopedStore)
+    }
+  }
   storeState[21] = patchStoreFn(storeGet)
   storeState[22] = scopedSet
   storeState[23] = patchStoreFn(storeSub)
