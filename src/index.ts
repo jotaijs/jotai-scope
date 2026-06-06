838f41682b0ff8bc565c55e0f7efce8b863ec9d3
Sean Wood <woodywoodsta@gmail.com>
2026-06-06 12:16:18 +0100

    fix: export any writable atom

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createIsolation } from './createIsolation'
 export { ScopeProvider } from './ScopeProvider/ScopeProvider'
 export { createScope } from './ScopeProvider/scope'
+export type { AnyWritableAtom } from './types'
